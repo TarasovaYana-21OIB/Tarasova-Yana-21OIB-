d6c97ab9f20defa7b6f20881c506a9e7be4a1f8e
TarasovaYana-21OIB <128086687+TarasovaYana-21OIB@users.noreply.github.com>
2023-12-22 15:50:49 +0300

    подсчет нулей.py

---
+++ подсчет нулей.py
@@ -0,0 +1,9 @@
+N = int(input("Введите число N: "))
+score = 0
+
+for i in range(N):
+  number = int(input("Введите целое число: "))
+  if number == 0
+     score += 1
+
+print("Количество нулей:", score)
