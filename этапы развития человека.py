e7c61d14f148ad6e7b7bca06a98e778425d9b39a
TarasovaYana-21OIB <128086687+TarasovaYana-21OIB@users.noreply.github.com>
2023-12-24 13:32:36 +0300

    этапы развития человека.py

---
+++ этапы развития человека.py
@@ -0,0 +1,13 @@
+stage1 = input("Введите первый этап развития человек: ")
+stages.append(stage1)
+
+stage2 = input("Введите второй этап развития человека: ")
+stages.append(stage2)
+
+stage3 = input("Введите третий этап развития человека: ")
+stages.append(stage3)
+
+stage4 = input("Введите четвертый этап развития человека: ")
+stages.append(stage4)
+result = " =>".join(stages)
+print(result)
